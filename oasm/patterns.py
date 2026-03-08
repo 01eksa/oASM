e0324836f3ed8f1c41c21621ff50ed175910fc24
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-03-08 16:58:45 +0200

    - Add arrays - Add support for creating repeating strings and arrays

--- oasm/patterns.py
+++ oasm/patterns.py
@@ -1,12 +1,15 @@
 import re
 
-
 BLANK = re.compile(r'\s+')
 
 INT = re.compile(r'[-+]?[0-9]+')
 INT16 = re.compile(r'[-+]?0x[0-9a-fA-F]+')
 FLOAT = re.compile(r'[-+]?[0-9]*\.[0-9]+')
-STRING = re.compile(r'"((?:[^"\\]|\\.)*)"')
+STRING = re.compile(r'"((?:[^"\\]|\\.)+)"')
+ARRAY = re.compile(r'\[(.+?)]')
+
+REP_STRING = re.compile(r'"((?:[^"\\]|\\.)+)"\s*\*\s*([0-9]+)')
+REP_ARRAY = re.compile(r'\[(.+?)]\s*\*\s*([0-9]+)')
 
 VAR = re.compile(r'[a-zA-Z_]\w*', re.ASCII)
 LABEL = re.compile(r'[a-zA-Z_]\w*:', re.ASCII)
