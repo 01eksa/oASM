5436b944a7520279b749864390615846c1498423
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-04-13 14:24:23 +0300

    Add CLI flags support to build_examples.py

--- build_examples.py
+++ build_examples.py
@@ -10,7 +10,7 @@ for file in os.listdir('./examples'):
         source = os.path.join('examples', file)
         target = os.path.join('bin', file.split('.')[0] + '.ovm')
         result = subprocess.run(
-                [sys.executable, '-m', 'oasm', source, target, '--rewrite'],
+                [sys.executable, '-m', 'oasm', source, target, '--rewrite', *sys.argv[1::]],
                 capture_output=True,
                 text=True
             )
@@ -18,4 +18,4 @@ for file in os.listdir('./examples'):
         if result.stdout:
             print(result.stdout)
         if result.stderr:
-            print(f"Error in {file}: {result.stderr}")
+            print(f"Error in {file}:\n{result.stderr}")
